25571117cbe240796c79cf36ed62aa97a53a6d1f
Misha <wrongsky1@yandex.ru>
2023-08-09 19:06:13 +0300

    add video

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -26,10 +26,11 @@ function Home() {
                 <iframe
                     width="560"
                     height="315"
-                    src="https://www.youtube.com/embed/wUEl8KrMz14"
+                    src="https://www.youtube.com/embed/R1l2EHD5DCI"
                     title="YouTube video player"
+                    frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen
+                    allowfullscreen
                 ></iframe>
                 <ContactForm />
                 <LogOutButton />
